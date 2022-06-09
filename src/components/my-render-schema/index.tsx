2a37c82517cb9259ba44bbdbfcde88f510a2a00b
崔晋龙01 <cuijinlong01@baijia.com>
2022-06-09 15:43:47 +0800

    feat: init

---
+++ src/components/my-render-schema/index.tsx
@@ -0,0 +1,4 @@
+import MyRenderSchema from './my-render-schema';
+
+export type { MyRenderSchemaProps } from './my-render-schema';
+export default MyRenderSchema;
---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+export type { ColorfulButtonProps } from './components/colorful-button';
+export { default as ColorfulButton } from './components/colorful-button';
+
+export type { ColorfulInputProps } from './components/colorful-input';
+export { default as ColorfulInput } from './components/colorful-input';
+
+export type { MyRenderSchemaProps } from './components/my-render-schema';
+export { default as MyRenderSchema } from './components/my-render-schema';
+
+const bizCssPrefix = 'bizpack';
+
+export {
+  bizCssPrefix
+}
